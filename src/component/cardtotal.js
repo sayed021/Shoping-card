b8453e9d0b4afbe6c16f6752656a57ebc8ba7b45
Md Abu Sayed <mdabusayedkpi@gmail.com>
2020-02-03 19:29:28 +0600

    Add files via upload

--- src/component/cardtotal.js
+++ src/component/cardtotal.js
@@ -5,21 +5,28 @@ const CardTotal = (props) => {
         <div className="card-total-wrap d-flex flex-wrap">
             <div className="ac-row w-100 d-flex justify-content-between pb-3">
                 <div>Subtotal</div>
-                <div>&#36; 1550 / yr</div>
+                <div>&#36; {props.subTotal.toFixed(2)} / yr</div>
             </div>
             <div className="ac-row w-100 d-flex justify-content-between pb-3">
-                <div>Tax</div>
-                <div>&#36; 100 / yr</div>
+                <div>Tax ( {props.tax}% ) </div>
+                <div>&#36;  {((props.subTotal * props.tax) / 100).toFixed(2)} / yr</div>
             </div>
             <div className="border-top pt-1 w-100 mt-2 mb-3"></div>
             <div className="ac-row w-100 d-flex justify-content-between pb-3">
                 <div>Total</div>
-                <div>&#36; 1550 / yr</div>
+                <div>&#36;  {(props.subTotal + (props.subTotal * props.tax) / 100).toFixed(2)} / yr</div>
             </div>
 
             <div className="ac-row w-100 pt-5">
                 <button type="button" className="btn btn-primary w-100">Start Exploring</button>
             </div>
+
+            <blockquote className="pt-3 mt-4">
+                <p>
+                    <strong>Note:</strong> {props.tax}% tax pay you in gov't fund{" "}
+                </p>
+                <strong>Thanks</strong>
+            </blockquote>
         </div>
     )
 }
--- src/component/checklist.js
+++ src/component/checklist.js
@@ -2,130 +2,48 @@ import React from 'react';
 
 const CheckList=(props)=> {
 
-    function getCheckItem(_items) {
-        return(
-            _items.map(_item=>{
-                return(
-                    <li key={_item.id}>
-                        <label className="check-group"  >
-                            <input type="checkbox" checked={_item.checked}  />
-                            <span>{_item.title}</span>
-                        </label>
-                    </li>
-                )
-            })
-        )
-    }
-
     return (
         <div className="group-wrap">
                 {
-                    (props.checkitems||[]).map(item => {
+                  (props.items||[]).map(item => {
                         return (
                             <ul className="group list-unstyled">
                                 <li key={item.id}>
                                     <label className="check-group"  >
-                                        <input type="checkbox" checked={item.checked}  />
-                                        <span>{item.title}</span>
+                                        <input
+                                            type="checkbox"
+                                            checked={item.checked}
+                                            onClick={e => props.parentChange(item)}
+                                        />{" "}
+                                        <span>{item.title}{" "}</span>
                                     </label>
 
-                                    {
-                                        item.children.length > 0 ? (
-                                            <ul className="group list-unstyled">
-                                                {
-                                                    getCheckItem(item.children)
-                                                }
-                                            </ul>
-                                        ) : ('')
-                                    } 
+                                    {item.subCatagory && item.subCatagory.length > 0 ? (
+                                        <ul className="group list-unstyled">
+                                            {item.subCatagory.map(cItem => {
+                                                return (
+                                                    <li key={cItem.id}>
+
+                                                        <label className="check-group" >
+                                                            <input
+                                                                type="checkbox"
+                                                                checked={cItem.checked}
+                                                                onClick={e => props.productChange(cItem.id)}
+                                                            />{" "}
+                                                            <span>{cItem.title} - ( ${cItem.price}){" "}</span>
+                                                        </label>
+                                                    </li>
+                                                );
+                                            })}
+                                        </ul>
+                                    ) : (
+                                            ""
+                                        )}
                                 </li>
                             </ul>
                         )
                     })
                 }
-            {/* <ul className="group list-unstyled">
-
-                <li>
-                    <label className="check-group">
-                        <input type="checkbox" checked />
-                        <span>IDARE Works</span>
-                    </label>
-                    <ul className="group list-unstyled">
-                        <li>
-                            <label className="check-group">
-                                <input type="checkbox" checked />
-                                <span>Project Management</span>
-                            </label>
-                        </li>
-                        <li>
-                            <label className="check-group">
-                                <input type="checkbox" checked />
-                                <span>Project Management</span>
-                            </label>
-                        </li>
-                        <li>
-                            <label className="check-group">
-                                <input type="checkbox" />
-                                <span>Project Management</span>
-                            </label>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-
-            <ul className="group list-unstyled">
-                <li>
-                    <label className="check-group">
-                        <input type="checkbox" checked />
-                        <span>Subsea Engineering Design</span>
-                    </label>
-                    <ul className="group list-unstyled">
-                        <li>
-                            <label className="check-group">
-                                <input type="checkbox" checked />
-                                <span>Conceptual Design</span>
-                            </label>
-                        </li>
-                        <li>
-                            <label className="check-group">
-                                <input type="checkbox" checked />
-                                <span>Allowable span</span>
-                            </label>
-                        </li>
-                        <li>
-                            <label className="check-group">
-                                <input type="checkbox" checked />
-                                <span>Up-heaval Buckling calculation</span>
-                            </label>
-                        </li>
-                        <li>
-                            <label className="check-group">
-                                <input type="checkbox" />
-                                <span>On Bottom Roughness</span>
-                            </label>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-
-            <ul className="group list-unstyled">
-                <li>
-                    <label className="check-group">
-                        <input type="checkbox"  />
-                        <span>Digital twin creator</span>
-                    </label>
-                </li>
-            </ul>
-
-            <ul className="group list-unstyled">
-                <li>
-                    <label className="check-group">
-                        <input type="checkbox"  />
-                        <span>Onshore asset integrity</span>
-                    </label>
-                </li>
-            </ul> */}
-
         </div>
     )
 }
