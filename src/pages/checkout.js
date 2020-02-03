f980111becd0d4a28d5018a58c3d270fb0b17e4f
Md Abu Sayed <mdabusayedkpi@gmail.com>
2020-02-03 19:29:03 +0600

    Add files via upload

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -4,124 +4,154 @@ import CheckList from '../component/checklist';
 
 const Checkout = (props) => {
 
-    const [checkList,setCheckList] = useState([]);
+    const [items,setItems] = useState([]);
 
     useEffect(()=>{
-        setCheckList([
+        setItems([
             {
                 id:1,
                 name:'check1',
-                title: 'IDARE Works',
-                checked: true,
-                children:[
+                title: 'Cetagory 1',
+                checked: false,
+                subCatagory:[
                     {
                         id: 2,
                         name: 'check2',
-                        title: 'Project Management',
+                        title: 'Product 1',
                         checked: true,
-                        children: []
+                        price: 100
                     },
                     {
                         id: 3,
                         name: 'check3',
-                        title: 'Project Management',
+                        title: 'Product 3',
                         checked: true,
-                        children: []
+                        price: 500
                     },
                     {
                         id: 4,
                         name: 'check4',
-                        title: 'Project Management',
+                        title: 'Product 9',
                         checked: false,
-                        children: []
+                        price: 300
                     }
                 ]
             },
             {
                 id:5,
-                name:'check5',
-                title: 'Subsea Engineering Design',
-                checked: true,
-                children:[
+                name:'Cetagory 2',
+                title: 'Cetagory 2',
+                checked: false,
+                subCatagory:[
                     {
                         id: 6,
                         name: 'check6',
                         title: 'Conceptual Design',
                         checked: true,
-                        children: []
+                        price: 50.69
                     },
                     {
                         id: 7,
                         name: 'check7',
                         title: 'Allowable Span',
                         checked: true,
-                        children: []
+                        price: 10.99
                     },
                     {
                         id: 8,
                         name: 'check8',
                         title: 'Up-Heaval Buckling Calculation',
                         checked: true,
-                        children: []
+                        price: 1000.10
                     },
                     {
                         id: 9,
                         name: 'check9',
                         title: 'On Bottom Roughness',
                         checked: false,
-                        children: []
+                        price: 150
                     }
                 ]
             },
             {
                 id: 10,
-                name:'check10',
-                title: 'Digital Twin Creator',
-                checked: true,
-                children:[]
+                name:'Cetagory 3',
+                title: 'Cetagory 3',
+                checked: false,
+                subCatagory:[]
             },
             {
                 id: 11,
-                name:'check11',
-                title: 'Onshore Asset Integrity',
+                name:'Cetagory 4',
+                title: 'Cetagory 69',
                 checked: false,
-                children:[]
+                subCatagory:[]
             },
         ])
     },[]);
 
-
-    // const findItem = (array, id) => {
-    //     var result;
-    //     array.some(o => result = o.id === id ? o : findItem(o.children || [], id));
-    //     return result;
-    // };
-
-    const checkChange =(i)=> {
-
-        // findItem(checkList, i)
-
-
-
-        setCheckList(
-            checkList.map((item) => {
-                if (item.id === i) {
-                    item.checked = !item.checked;
-                } else {
-                    if(item.children.length > 0) {
-
-                        item.children.map((c_item) => {
-
-                            if (c_item.id === i) {
-                                item.checked = !item.checked;
-                            }
-
-                        });
+    const checkChangeHandaller = async id => {
+        let newItems = (items || []).map(item => {
+            if (item.id === id) {
+                item.checked = !item.checked;
+            }
+            if (item.subCatagory && item.subCatagory.length > 0) {
+                (item.subCatagory || []).map(nItem => {
+                    if (nItem.id === id) {
+                        nItem.checked = !nItem.checked;
                     }
+                    return nItem;
+                });
+            }
+
+            if (item.subCatagory && item.subCatagory.length > 0) {
+                let count = 0;
+
+                item.subCatagory.map(itm => {
+                    if (itm.checked) {
+                        count += 1;
+                    }
+                    return itm;
+                });
+
+                if (item.subCatagory.length === count) {
+                    item.checked = true;
+                } else {
+                    item.checked = false;
                 }
-          })
-        );
-      };
+            }
+            return item;
+        });
+        setItems(newItems);
+    };
+
+    const parentChange = item => {
+        let newItem = (items || []).map(pItem => {
+            if (item.id === pItem.id) {
+                item.checked = !item.checked;
+                (pItem.subCatagory || []).map(cItem => {
+                    cItem.checked = item.checked;
+                    return cItem;
+                });
+            }
+            return pItem;
+        });
+        setItems(newItem);
+    };
+
+    const getTotal = () => {
+        let totalAmount = 0;
+
+        (items || []).map(pItem => {
+            (pItem.subCatagory || []).map(nItem => {
+                totalAmount += nItem.checked ? nItem.price : 0;
+                return nItem;
+            });
+            return pItem;
+        });
+
+        return totalAmount;
+    };
 
     return (
         <div className="wrap">
@@ -129,10 +159,18 @@ const Checkout = (props) => {
                 <div className="page-content checkout-page">
                     <h2 className="page-heading">Simple checkout page</h2>
                     <div className="checklist">
-                        <CheckList checkitems={checkList} checkChange={checkChange} />
+
+                        <CheckList 
+                            items={items} 
+                            checkChange={parentChange} 
+                            productChange={checkChangeHandaller} 
+                            parentChange={parentChange}
+                           
+                        />
+
                     </div>
                     <div className="check-summary">
-                        <CardTotal />
+                        <CardTotal subTotal={getTotal()} tax={15} />
                     </div>
                 </div>
             </div>
